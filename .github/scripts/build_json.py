9594c308fd25ae1bc4a4ad5b98e7c1cffc1ffe07
artvandelayio <mattmerrickbiz@gmail.com>
2024-10-25 23:10:06 -0400

    updated

--- .github/scripts/build_json.py
+++ .github/scripts/build_json.py
@@ -1,8 +1,22 @@
 import yaml
 import json
+import os
 
-with open('_data/tools.yml', 'r') as yaml_file:
+# Get the directory of the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the YAML file
+yaml_path = os.path.join(script_dir, '..', '..', '_data', 'tools.yml')
+
+# Construct the path for the output JSON file
+json_path = os.path.join(script_dir, '..', '..', 'tools.json')
+
+# Read the YAML file
+with open(yaml_path, 'r') as yaml_file:
     tools = yaml.safe_load(yaml_file)
 
-with open('tools.json', 'w') as json_file:
+# Write the JSON file
+with open(json_path, 'w') as json_file:
     json.dump(tools, json_file, indent=2)
+
+print(f"JSON file created at: {json_path}")
